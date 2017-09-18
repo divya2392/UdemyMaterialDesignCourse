46fcecc32e967f8053f2a6eb1ab81d64831b8821
Divya Gupta <divagupta2392@gmail.com>
2017-09-18 17:04:14 +0530

    created bottom navigation

---
+++ app/src/main/java/com/divya/udemymaterialdesign/BottomnavActivityPartOne.java
@@ -0,0 +1,14 @@
+package com.divya.udemymaterialdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BottomnavActivityPartOne extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottomnavigation_partone);
+    }
+}
